1c1556c0d4a6ba9de2bb3a74bf6d2f1b5f8324ba
1361356742@qq.vom <1361356742@qq.com>
2019-12-17 15:31:09 +0800

    爬取网络图片

---
+++ DownloadImage.py
@@ -0,0 +1,14 @@
+#爬取网络图片
+
+import random
+import urllib.request
+
+
+def download_web_image(url):
+    name = random.randrange(1, 1000)
+    full_name = str(name) + '.jpg'
+    urllib.request.urlretrieve(url, full_name)
+
+
+download_web_image(
+    "http://g.hiphotos.baidu.com/zhidao/pic/item/c83d70cf3bc79f3d6e7bf85db8a1cd11738b29c0.jpg")

6193d4ef7298736c50ca0d472437136715df669e
1361356742@qq.vom <1361356742@qq.com>
2019-12-17 14:09:56 +0800

    测试

--- ExampleTest.py
+++ ExampleTest.py
@@ -9,3 +9,12 @@ if (var == 100):
     print("变量var的值为100")
     print("Good bye")
 
+from collections import Counter
+
+text = "dada 年后 你您 当年我都 道德经澳门 解决房价 ii 哦 没 跑跑 年后"
+words = text.split()
+print(words)
+
+counter = Counter(words)
+topThree = counter.most_common(3)# 出现次数最多的三个元素 没有特定顺序 （获取字符串中最常见的文字）
+print(topThree)
